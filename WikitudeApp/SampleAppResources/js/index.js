dcfe4a980e9cd5d4f21cfbc26e408096e96e7640
Hugo Torzuoli <torzuoli.hugo@gmail.com>
2017-03-16 02:12:42 +0100

    wikitude installer modified -> Using a sample with Wikitude SDK Cordova

---
+++ WikitudeApp/SampleAppResources/js/index.js
@@ -0,0 +1,159 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var app = {
+
+    // represents the device capability of launching ARchitect Worlds with specific features
+    isDeviceSupported: false,
+    isArchitectWorldLoaded: false,
+
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    onDeviceReady: function() {
+        app.wikitudePlugin = cordova.require("com.wikitude.phonegap.WikitudePlugin.WikitudePlugin");
+        // set a callback for android that is called once the back button was clicked.
+        if ( cordova.platformId == "android" ) {
+            app.wikitudePlugin.setBackButtonCallback(app.onBackButton);
+        }
+        app.wikitudePlugin.setOnUrlInvokeCallback(app.onUrlInvoke);
+    },
+    // --- Wikitude Plugin ---
+    loadExampleARchitectWorld: function(example) {
+
+        app.isArchitectWorldLoaded = false;
+        // inject poi data using phonegap's GeoLocation API and inject data using World.loadPoisFromJsonData
+        if ( example.requiredExtension === "ObtainPoiDataFromApplicationModel" ) {
+            prepareApplicationDataModel();
+        }
+
+        /* cordova.file.applicationDirectory is used to demonstrate the use of the cordova file plugin in combination with the Wikitude plugin */
+        /* The length check here is only necessary because for each example the same 'example' object is given here and we only want to change the path once. */
+        if ( example.path.length > cordova.file.applicationDirectory ) {
+            if ( example.path.substring(0, cordova.file.applicationDirectory) != cordova.file.applicationDirectory ) {
+                example.path = cordova.file.applicationDirectory + example.path;
+            }
+        }
+
+        app.prepareArchitectWorld(example, function() {
+            app.loadARchitectWorld(example);
+        });
+    },
+    loadCustomARchitectWorldFromURL: function(url) {
+        var customArchitectWorld = {
+            "path": url,
+            "requiredFeatures": [
+                "image_tracking",
+                "geo"
+            ],
+            "startupConfiguration": {
+                "camera_position": "back"
+            }
+        };
+        app.isArchitectWorldLoaded = false;
+        app.prepareArchitectWorld(customArchitectWorld, function() {
+            app.loadARchitectWorld(customArchitectWorld);
+        });
+    },
+    prepareArchitectWorld: function(architectWorld, successCallback) {
+        app.wikitudePlugin.isDeviceSupported(function() {
+            app.wikitudePlugin.requestAccess(
+                function() {
+                    successCallback();
+                },
+                function(error) {
+                    /* The error object contains two error messages.
+                        * userDescription is a end user formatted message that can be displayed with e.g. a JS alert
+                        * developerDescription is a developer formatted message with more detailed information about the error
+                     */
+                    /* Here, the userDescription is used to show a confirmation box which, in case of a positive result, shows the applications settings so that user can grant access. */
+                    var openAppSettings = confirm(error.userDescription + '\nOpen App Settings?');
+                    if ( openAppSettings == true ) {
+                        app.wikitudePlugin.openAppSettings();
+                    }
+                },
+                architectWorld.requiredFeatures);
+        }, function(errorMessage) {
+            alert(errorMessage);
+        },
+        architectWorld.requiredFeatures);
+    },
+    // Use this method to load a specific ARchitect World from either the local file system or a remote server
+    loadARchitectWorld: function(architectWorld) {
+        app.wikitudePlugin.loadARchitectWorld(function successFn(loadedURL) {
+                /* Respond to successful world loading if you need to */
+                app.isArchitectWorldLoaded = true;
+
+                /* in case the loaded Architect World belongs to the 'obtain poi data from application model' example, we can now safely inject poi data. */
+                if ( architectWorld.requiredExtension === "ObtainPoiDataFromApplicationModel" ) {
+                    injectGeneratedPoiJsonData();
+                }
+            }, function errorFn(error) {
+                app.isArchitectWorldLoaded = false;
+                alert('Loading AR web view failed: ' + error);
+            },
+            architectWorld.path, architectWorld.requiredFeatures, architectWorld.startupConfiguration
+        );
+    },
+    // This function gets called if you call "document.location = architectsdk://" in your ARchitect World
+    onUrlInvoke: function (url) {
+        if (url.indexOf('captureScreen') > -1) {
+            app.wikitudePlugin.captureScreen(
+                function(absoluteFilePath) {
+                    alert("snapshot stored at:\n" + absoluteFilePath);
+                },
+                function (errorMessage) {
+                    alert(errorMessage);
+                },
+                true, null
+            );
+        } else {
+            alert(url + "not handled");
+        }
+    },
+    onBackButton: function() {
+        /* Android back button was pressed and the Wikitude PhoneGap Plugin is now closed */
+    },
+    showBuildInformation: function() {
+        var sdkVersion = ""
+
+        app.wikitudePlugin.getSDKVersion(function(version){ sdkVersion = version });
+
+        app.wikitudePlugin.getSDKBuildInformation(function(buildInformationJSON) {
+            var buildInformation = JSON.parse(buildInformationJSON);
+            alert(
+                "Build configuration: " + buildInformation.buildConfiguration + "\n" +
+                "Build date: " + buildInformation.buildDate + "\n" +
+                "Build number: " + buildInformation.buildNumber + "\n" +
+                "Build version: " + sdkVersion
+            );
+        });
+    }
+    // --- End Wikitude Plugin ---
+};
+
+app.initialize();
---
+++ WikitudeApp/Scripts/WikitudePhoneGapSampleGenerator.sh
@@ -0,0 +1,153 @@
+#!/bin/sh
+#
+#  Wikitude_PhoneGap_Sample_Generator.sh
+#
+#  Created by Wikitude GmbH on 03/26/14.
+#
+#
+
+set -e
+
+PROJECT_DIRECTORY=$1
+PROJECT_ID=$2
+PROJECT_NAME=$3
+
+BUILD_IOS=$4
+BUILD_ANDROID=$5
+
+SOURCE_DIRECTORY=$6
+DESTINATION_DIRECTORY="$PROJECT_DIRECTORY/www"
+
+USE_PLUGIN_SOURCE=false
+
+if [ "$#" -eq 7 ]; then
+	USE_PLUGIN_SOURCE=true
+	PLUGIN_SOURCE=$7
+fi
+
+
+## Run some checks if we can proceed...
+if [ -d $PROJECT_DIRECTORY ]; then
+	echo "Project directory already exists"
+	exit 1
+fi
+
+
+## Generate sample project ##
+echo "*** GENERATING PROJECT ***"
+
+# Create the project directory
+cordova create $PROJECT_DIRECTORY $PROJECT_ID "$PROJECT_NAME"
+sed -i '.original' 's/\<platform name="android"\>/\<\platform name="android"\>\<preference name="android-minSdkVersion" value="16"\/\>/g' "${PROJECT_DIRECTORY}"/config.xml
+
+# Step into the created project directory
+cd $PROJECT_DIRECTORY
+
+
+## Copy all sample relevant resources ##
+echo "*** PREPARING SAMPLE CONTENT ***"
+
+# copy css
+cp -R "${SOURCE_DIRECTORY}"/css/* "${DESTINATION_DIRECTORY}"/css
+
+# copy jquery
+cp -R "${SOURCE_DIRECTORY}"/jquery "${DESTINATION_DIRECTORY}"
+
+# copy js
+cp -R "${SOURCE_DIRECTORY}"/js/* "${DESTINATION_DIRECTORY}"/js
+
+# add samples
+cp -R "${SOURCE_DIRECTORY}"/world "${DESTINATION_DIRECTORY}"/
+
+# copy index.html
+cp -R "${SOURCE_DIRECTORY}"/index.html "${DESTINATION_DIRECTORY}"/index.html
+
+
+## Build sample app
+echo "*** BUILDING SAMPLE APP ***"
+
+if [ "true" == "$BUILD_IOS" ]; then
+	echo "iOS (cordova version '"$(cordova --version)"')"
+	cordova platform add ios
+
+	# copy app icons
+	ICON_DESTINATION_PATH="${DESTINATION_DIRECTORY}"/../platforms/ios/"${PROJECT_NAME}"/Resources/icons
+	if ! [ -d "${ICON_DESTINATION_PATH}" ]; then
+		# cordova version 6
+    	ICON_DESTINATION_PATH="${DESTINATION_DIRECTORY}"/../platforms/ios/"${PROJECT_NAME}"/Images.xcassets
+    	rm -r "$ICON_DESTINATION_PATH"/AppIcon.appiconset
+    	cp -R "${SOURCE_DIRECTORY}"/icons/ios/cordova_6/ "$ICON_DESTINATION_PATH"
+    else
+    	# older cordova versions
+		rm -r "$ICON_DESTINATION_PATH"/*
+		cp -R "${SOURCE_DIRECTORY}"/icons/ios/cordova_5/* "$ICON_DESTINATION_PATH"
+	fi
+fi
+if [ "true" == "$BUILD_ANDROID" ]; then
+	echo "Android (cordova version '"$(cordova --version)"')"
+	cordova platform add android@5.0.0
+
+	# copy app icons
+	ICON_DESTINATION_PATH="${DESTINATION_DIRECTORY}"/../platforms/android/res
+	cp -a "${SOURCE_DIRECTORY}"/icons/android/* "$ICON_DESTINATION_PATH"
+fi
+
+
+## Add Wikitude plugin
+echo "*** ADDING WIKITUDE PLUGIN ***"
+
+if [ "true" == "$USE_PLUGIN_SOURCE" ]; then
+	cordova plugin add $PLUGIN_SOURCE
+else
+	echo "Fetching plugin from default GitHub master"
+	cordova plugin add https://github.com/Wikitude/wikitude-cordova-plugin.git
+fi
+
+
+## Add Cordova File plugin
+cordova plugin add cordova-plugin-file
+
+
+# Install Wikitude SDK license
+echo "** ADDING WIKITUDE SDK LICENSE ***"
+
+# Read in the license key
+LICENSE_FILE="${SOURCE_DIRECTORY}"/../com.wikitude.phonegapsamples.lic.signed
+LICENSE_KEY=`cat "${LICENSE_FILE}"`
+
+if [ "true" == "$BUILD_IOS" ]; then
+	# Replace license key for iOS
+
+	INPUT_FILES[0]=$PROJECT_DIRECTORY/platforms/ios/platform_www/plugins/com.wikitude.phonegap.WikitudePlugin/www/WikitudePlugin.js
+	INPUT_FILES[1]=$PROJECT_DIRECTORY/platforms/ios/www/plugins/com.wikitude.phonegap.WikitudePlugin/www/WikitudePlugin.js
+	INPUT_FILES[2]=$PROJECT_DIRECTORY/plugins/com.wikitude.phonegap.WikitudePlugin/www/WikitudePlugin.js
+
+    for INPUT_FILE in "${INPUT_FILES[@]}"
+    do
+        sed -i.bak -e "s/ENTER-YOUR-KEY-HERE/${LICENSE_KEY//\//\/}/g" $INPUT_FILE && rm $INPUT_FILE.bak
+    done
+
+    # Add location access description key/value to info.plist
+	/usr/libexec/PlistBuddy -c "Add :NSLocationWhenInUseUsageDescription string 'Access to your current location information (GPS) is needed to find the nearest points of interest around your current position.'" "${DESTINATION_DIRECTORY}"/../platforms/ios/"${PROJECT_NAME}"/"${PROJECT_NAME}"-Info.plist
+
+	# Add camera access description key/value to info.plist
+	/usr/libexec/PlistBuddy -c "Add :NSCameraUsageDescription string 'Access to the camera is needed to display augmented reality content on top of your camera image.'" "${DESTINATION_DIRECTORY}"/../platforms/ios/"${PROJECT_NAME}"/"${PROJECT_NAME}"-Info.plist
+
+	# Add photo library access description key/value to info.plist
+	/usr/libexec/PlistBuddy -c "Add :NSPhotoLibraryUsageDescription string 'Access to your photo library is required for the example 'Bonus: Capture Screen' because it adds a screenshot of the current Architect view.'" "${DESTINATION_DIRECTORY}"/../platforms/ios/"${PROJECT_NAME}"/"${PROJECT_NAME}"-Info.plist
+fi
+
+if [ "true" == "$BUILD_ANDROID" ]; then
+	# ... and Android
+
+	INPUT_FILES[0]=$PROJECT_DIRECTORY/platforms/android/platform_www/plugins/com.wikitude.phonegap.WikitudePlugin/www/WikitudePlugin.js
+	INPUT_FILES[1]=$PROJECT_DIRECTORY/platforms/android/assets/www/plugins/com.wikitude.phonegap.WikitudePlugin/www/WikitudePlugin.js
+	INPUT_FILES[2]=$PROJECT_DIRECTORY/plugins/com.wikitude.phonegap.WikitudePlugin/www/WikitudePlugin.js
+
+    for INPUT_FILE in "${INPUT_FILES[@]}"
+    do
+        sed -i.bak -e "s/ENTER-YOUR-KEY-HERE/${LICENSE_KEY//\//\/}/g" $INPUT_FILE && rm $INPUT_FILE.bak
+    done
+fi
+
+echo "*** DONE - SUCCESS ***"
