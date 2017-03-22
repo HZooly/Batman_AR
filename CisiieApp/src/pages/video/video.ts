ed54e472590581727ad3517f39eb217388f43749
Hugo Torzuoli <torzuoli.hugo@gmail.com>
2017-03-22 16:35:38 +0100

    video added in a tab

---
+++ CisiieApp/src/pages/video/video.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'video',
+  templateUrl: 'video.html'
+})
+export class VideoPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
