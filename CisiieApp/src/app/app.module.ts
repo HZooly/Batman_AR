143579f171ce8693cf83a5ae60f106c1c51899ad
Hugo Torzuoli <torzuoli.hugo@gmail.com>
2017-03-29 16:17:24 +0200

    delete others useless folders and relations in tabs.ts

--- CisiieApp/src/app/app.module.ts
+++ CisiieApp/src/app/app.module.ts
@@ -1,8 +1,7 @@
-import { NgModule, ErrorHandler } from '@angular/core'; 
+import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 import { ARView } from '../pages/ar-view/ar-view';
@@ -11,7 +10,6 @@ import { ARView } from '../pages/ar-view/ar-view';
   declarations: [
     MyApp,
     AboutPage,
-    ContactPage,
     HomePage,
     TabsPage,
     ARView
@@ -23,7 +21,6 @@ import { ARView } from '../pages/ar-view/ar-view';
   entryComponents: [
     MyApp,
     AboutPage,
-    ContactPage,
     HomePage,
     TabsPage,
     ARView
--- CisiieApp/src/pages/tabs/tabs.ts
+++ CisiieApp/src/pages/tabs/tabs.ts
@@ -3,7 +3,6 @@ import { Tabs} from 'ionic-angular';
 import { HomePage } from '../home/home';
 import { ARView } from '../ar-view/ar-view';
 import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
 
 @Component({
   templateUrl: 'tabs.html'
@@ -17,7 +16,6 @@ export class TabsPage {
   tab1Root: any = HomePage;
   tab2Root: any = ARView;
   tab3Root: any = AboutPage;
-  tab4Root: any = ContactPage;
 
   constructor() {
   }
